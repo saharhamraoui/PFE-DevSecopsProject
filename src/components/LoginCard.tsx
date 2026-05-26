8e31b605991509b8b8581bf1245dcafae3bf2070
saharhamraoui <sahar.hamraoui@esprit.tn>
2026-05-26 07:13:35 +0200

    change login page text

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -50,7 +50,7 @@ export const LoginCard = () => {
             4YOU
           </div>
           <p className="text-lg text-card-foreground">
-            <span className="font-semibold">Bienvenue Sahar sur 4YOU,</span>{" "}
+            <span className="font-semibold">Bienvenue Sahar sur 4YOU,This is a demo video </span>{" "}
             connectez-vous pour accéder à votre espace.
           </p>
         </div>
